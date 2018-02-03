60abca4ae9ee8a97409942554b79566f06913af2
kasecato <kasecato@gmail.com>
2018-02-03 20:11:33 +0900

    update redpen 1.8.0 to 1.9.0

--- src/cc/redpen/validator/sentence/MSStyleJPValidator.java
+++ src/cc/redpen/validator/sentence/MSStyleJPValidator.java
@@ -4,29 +4,26 @@ import cc.redpen.model.Sentence;
 import cc.redpen.validator.Validator;
 import cc.redpen.validator.sentence.domain.style.MSStyleJP;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
-
-import static java.util.Collections.singletonList;
 
 public class MSStyleJPValidator extends Validator implements MSStyleJP {
 
     @Override
     public List<String> getSupportedLanguages() {
-        return singletonList(Locale.JAPANESE.getLanguage());
+        return Collections.singletonList(Locale.JAPANESE.getLanguage());
     }
 
     @Override
-    public void validate(Sentence sentence) {
+    public void validate(final Sentence sentence) {
 
         validateMSStyle(sentence)
                 .stream()
                 //.parallel()
-                .forEach(result
-                        -> result.getErrorPositions()
-                        .stream()
-                        .forEach(position
-                                -> addErrorWithPosition(result.getErrorMessage(), sentence, position.row, position.col)));
+                .forEach(result -> result.getErrorPositions().stream()
+                        //.parallel()
+                        .forEach(position -> addErrorWithPosition(result.getErrorMessage(), sentence, position.row, position.col)));
 
     }
 
